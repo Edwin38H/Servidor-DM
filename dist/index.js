82a6bbb38507f3cbffb1e0500b6b303a4422a929
Edwin38H <djerizooriginal@gmail.com>
2022-11-18 10:56:15 -0600

    Practica 30

--- dist/index.js
+++ dist/index.js
@@ -4,23 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
-const user_1 = __importDefault(require("./routes/user"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const user_1 = __importDefault(require("./routes/user"));
+const post_1 = __importDefault(require("./routes/post"));
 const server = new server_1.default();
 //Body parser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
 //Rutas de mi app
 server.app.use('/user', user_1.default);
+server.app.use('/posts', post_1.default);
 //conectar BD
 server.start(() => {
-    console.log(`Servidor corriendo en puerto ${server.port}`);
-    const mongoose = require("mongoose");
+    const mongoose = require('mongoose');
     mongoose.connect('mongodb://localhost:27017/fotosgram2', { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
         if (err)
             throw err;
         console.log('Base de datos ONLINE2');
     });
-    //levantar express
-    console.log(`Servidor corriendo en puerto ${server.port}`);
 });
+//levantar express
+console.log(`Servidor corriendo en puerto ${server.port}`);
--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
 import Server from './classes/server';
-import userRoutes from './routes/user';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import userRoutes from './routes/user';
+import postRoutes from './routes/post';
 const server = new Server();
 //Body parser
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
 //Rutas de mi app
 server.app.use('/user', userRoutes);
+server.app.use('/posts', postRoutes);
 //conectar BD
 server.start(() => {
-    console.log(`Servidor corriendo en puerto ${server.port}`);
-    const mongoose = require("mongoose");
-    mongoose.connect('mongodb://localhost:27017/fotosgram2',
-        { useUnifiedTopology: true, useNewUrlParser: true }, (err: any) => {
-            if (err) throw err;
-            console.log('Base de datos ONLINE2');
-        }
-    )
-    //levantar express
+  const mongoose = require('mongoose');
+ mongoose.connect('mongodb://localhost:27017/fotosgram2',
+    { useUnifiedTopology: true, useNewUrlParser: true}, (err:any) => {
+        if (err) throw err;
+        console.log('Base de datos ONLINE2');
+    }
+)
+})
+//levantar express
 
     console.log(`Servidor corriendo en puerto ${server.port}`);
-}
-);
